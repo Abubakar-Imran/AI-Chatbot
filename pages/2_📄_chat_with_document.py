0fd3e236a20bda66624d5b1ac3c90c49369b542d
Abubakar <abubakarimran553@gmail.com>
2024-08-28 18:54:03 +0500

    document chat using langchian

---
+++ pages/2_📄_chat_with_document.py
@@ -0,0 +1,105 @@
+import os
+import utils
+import streamlit as st
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import DocArrayInMemorySearch
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+
+
+st.set_page_config(page_title="ChatPDF", page_icon="📄")
+st.header('Chat with your documents (Basic RAG)')
+st.write('Has access to custom documents and can respond to user queries by referring to the content within those documents')
+
+class PyramidDocChatbot:
+
+    def __init__(self):
+        utils.sync_st_session()
+        self.llm = utils.configure_llm()
+        self.embedding_model = utils.configure_embedding_model()
+
+    def save_file(self, file):
+        folder = 'tmp'
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        
+        file_path = f'./{folder}/{file.name}'
+        with open(file_path, 'wb') as f:
+            f.write(file.getvalue())
+        return file_path
+
+    @st.spinner('Analyzing documents..')
+    def setup_qa_chain(self, uploaded_files):
+        docs = []
+        for file in uploaded_files:
+            file_path = self.save_file(file)
+            loader = PyPDFLoader(file_path)
+            docs.extend(loader.load())
+        
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200
+        )
+        splits = text_splitter.split_documents(docs)
+        vectordb = DocArrayInMemorySearch.from_documents(splits, self.embedding_model)
+
+        retriever = vectordb.as_retriever(
+            search_type='mmr',
+            search_kwargs={'k':2, 'fetch_k':4}
+        )
+
+        system_prompt = (
+            "You are an assistant for question-answering tasks. "
+            "Use the following pieces of retrieved context to answer "
+            "the question. If you don't know the answer, say that you "
+            "don't know."
+            "\n\n"
+            "{context}"
+        )
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", system_prompt),
+                ("human", "{input}"),
+            ]
+        )
+        question_answer_chain = create_stuff_documents_chain(self.llm, prompt)
+        rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+        return rag_chain
+
+    def main(self):
+
+        if "messages" not in st.session_state:
+            st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+        # for msg in st.session_state["messages"]:
+        #     st.chat_message(msg["role"]).write(msg["content"])
+        
+        # User Inputs
+        uploaded_files = st.sidebar.file_uploader(label='Upload PDF files', type=['pdf'], accept_multiple_files=True)
+        if not uploaded_files:
+            st.error("Please upload PDF documents to continue!")
+            st.stop()
+
+        user_query = st.chat_input(placeholder="Ask me anything!")
+
+        if uploaded_files and user_query:
+            qa_chain = self.setup_qa_chain(uploaded_files)
+            utils.display_msg(user_query, 'user')
+
+            response = qa_chain.invoke({"input": user_query})
+            st.session_state.messages.append({"role": "assistant", "content": response["answer"]})
+            st.chat_message("assistant").write(response["answer"])
+            utils.print_qa(PyramidDocChatbot, user_query, response)
+
+            # to show references
+            for idx, doc in enumerate(response['context'],1):
+                filename = os.path.basename(doc.metadata['source'])
+                page_num = doc.metadata['page']
+                ref_title = f":blue[Reference {idx}: *{filename} - page.{page_num}*]"
+                with st.popover(ref_title):
+                    st.caption(doc.page_content)
+
+if __name__ == "__main__":
+    obj = PyramidDocChatbot()
+    obj.main()
